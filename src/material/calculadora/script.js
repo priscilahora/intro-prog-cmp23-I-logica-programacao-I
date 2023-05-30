60b4a47105cb30d0c46bbca3ad42e9bbc907809f
Mayhhara Morais <69424067+mflilian@users.noreply.github.com>
2023-05-30 16:07:48 -0300

    Update script.js

--- src/material/calculadora/script.js
+++ src/material/calculadora/script.js
@@ -3,7 +3,7 @@ function calculadora(){
         let num1 = document.getElementById("valor1").value;
         let num2 = document.getElementById("valor2").value;
         
-        swal(`O valor total da soma é: ${total}`);
+        swal(`O valor total da soma é`);
 }
 
 //o Swal substitui o Alert, deixando o design mais bonito.
