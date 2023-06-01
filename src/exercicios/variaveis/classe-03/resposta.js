43297e4cac457c20b338fbb93c3081e41da600e9
mflilian <mayhharamorais@gmail.com>
2023-06-01 18:34:55 -0300

    misc: add exercises

---
+++ src/exercicios/variaveis/classe-03/resposta.js
@@ -0,0 +1,9 @@
+// Faça um programa que obtenha o ano de nascimento da usuária e retorne quantos anos ela tem.
+// ano atual - ano de nascimento = idade
+
+let anoAtual = 2023
+const anoDeNascimento = 2000
+
+let idade = anoAtual - anoDeNascimento;
+
+console.log(`Essa pessoa tem ${idade} anos de idade`)
--- src/material/calculadora/script.js
+++ src/material/calculadora/script.js
@@ -1,8 +1,14 @@
 function calculadora(){
-    
-        let num1 = document.getElementById("valor1").value;
-        let num2 = document.getElementById("valor2").value;
+
+        let numeroUm = document.getElementById("valor1").value;
+        let numeroDois = document.getElementById("valor2").value;
         
+        // tudo o que entra nos inputs é string, por isso é necessário converter para número.
+        let soma = (parseInt(numeroUm) / parseInt(numeroDois));
+
+        console.log("A soma é: " + soma);
+        
+        swal("A soma é: " + soma);
 }
 
 //o Swal substitui o Alert, deixando o design mais bonito.
