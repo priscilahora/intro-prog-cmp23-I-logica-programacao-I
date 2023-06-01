24a897a156a0fe7dd974f34fefdb49cf939a3bcc
mflilian <mayhharamorais@gmail.com>
2023-06-01 18:34:34 -0300

    misc: add exercises

---
+++ src/exercicios/variaveis/classe-05/resposta.js
@@ -0,0 +1,15 @@
+// Faça um programa que armazene duas frases e compare se elas são iguais E IMPRIMA.
+
+const fraseUm = "Eu gosto de batata"
+const fraseDois = "Eu gosto de batata"
+
+let comparacaoFrases = (fraseUm == fraseDois)
+
+console.log(`As frases são iguais? ${comparacaoFrases}`)
+
+let numeroUm = 10
+let numeroDois = 10
+
+let comparacaoNumeros = (numeroUm != numeroDois)
+
+console.log(`Os numeros sao diferentes? ${comparacaoNumeros}`)
